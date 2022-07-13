82a2eef02869d47726b971c925532c89bb789be8
NikitaM123 <[nikitamalihin123@gmail.com]>
2022-07-13 21:27:13 +0300

    fix

--- src/main/java/PatShelter/ConsoleApplication.java
+++ src/main/java/PatShelter/ConsoleApplication.java
@@ -1,3 +1,10 @@
+package PatShelter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ConsoleApplication {
-    
+    public static void main(String[] args) {
+        SpringApplication.run(ConsoleApplication.class, args);
+    }
 }
